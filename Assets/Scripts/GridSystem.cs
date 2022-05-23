98e9fdeae75cd25910e6429da9ea8583d7796d06
paisaningk <gaw.fd@hotmail.com>
2022-05-24 05:28:45 +0700

    add tile map and Move unit

--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -2,67 +2,56 @@
 using Unity.Mathematics;
 using UnityEngine;
 
-namespace Gird2D
+[RequireComponent(typeof(MoveUnit))]
+public class GridSystem : MonoBehaviour
 {
-    public class GridSystem : MonoBehaviour
+    [SerializeField] private float cellSizeX;
+    [SerializeField] private float cellSizeY;
+    [SerializeField] private GameObject mainTerrain;
+
+    internal void MoveUnitToPosition(GameObject unit)
     {
-        [SerializeField] private float cellSizeX;
-        [SerializeField] private float cellSizeY;
-        [SerializeField] private GameObject mainTerrain;
-        [SerializeField] private GameObject test;
-
-        public void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                MoveGameObjectFollowMouse();
-            }
-        }
-
-        private void MoveGameObjectFollowMouse()
-        {
-            test.transform.position = GetPositionFromIndex(RaycastCheckGetRaycastHit().point);
-        }
-
-        private RaycastHit RaycastCheckGetRaycastHit()
-        {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Physics.Raycast(ray, out var hit);
-            return hit;
-        }
-        
-
-        private Vector3 GetPositionFromIndex(Vector3 position)
-        {
-            GetCellIndex(position, out var indexX, out var indexY);
-            GetCellCenterPos(indexX, indexY, out var newPosition);
-
-            return newPosition;
-        }
-
-        private void GetCellIndex(Vector3 position, out int indexX, out int indexY)
-        {
-            Vector3 transformPosition = mainTerrain.transform.position;
-            indexX = Mathf.FloorToInt((position.x - transformPosition.x) / cellSizeX);
-            indexY = Mathf.FloorToInt((position.y - transformPosition.y) / cellSizeY);
-
-        }
-
-        private void GetCellCornerPos(int indexX, int indexY, out Vector3 position)
-        {
-            Vector3 transformPosition = mainTerrain.transform.position;
-            position.x = (indexX * cellSizeX + transformPosition.x);
-            position.y = (indexY * cellSizeY + transformPosition.y);
-            position.z = 0.1f;
-        }
-
-        private void GetCellCenterPos(int indexX, int indexY,out Vector3 position)
-        {
-            GetCellCornerPos(indexX, indexY, out position);
-            
-            position.x += cellSizeX / 2;
-            position.y += cellSizeX / 2;
-        }
-        
+        unit.transform.position = GetPositionFromIndex(GetRaycastHit().point);
     }
+
+    private RaycastHit GetRaycastHit()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out var hit);
+        return hit;
+    }
+
+
+    private Vector3 GetPositionFromIndex(Vector3 position)
+    {
+        GetCellIndex(position, out var indexX, out var indexY);
+        GetCellCenterPos(indexX, indexY, out var newPosition);
+
+        return newPosition;
+    }
+
+    private void GetCellIndex(Vector3 position, out int indexX, out int indexY)
+    {
+        Vector3 transformPosition = mainTerrain.transform.position;
+        indexX = Mathf.FloorToInt((position.x - transformPosition.x) / cellSizeX);
+        indexY = Mathf.FloorToInt((position.y - transformPosition.y) / cellSizeY);
+
+    }
+
+    private void GetCellCornerPos(int indexX, int indexY, out Vector3 position)
+    {
+        Vector3 transformPosition = mainTerrain.transform.position;
+        position.x = (indexX * cellSizeX + transformPosition.x);
+        position.y = (indexY * cellSizeY + transformPosition.y);
+        position.z = 0.1f;
+    }
+
+    private void GetCellCenterPos(int indexX, int indexY,out Vector3 position)
+    {
+        GetCellCornerPos(indexX, indexY, out position);
+            
+        position.x += cellSizeX / 2;
+        position.y += cellSizeX / 2;
+    }
+        
 }
---
+++ Assets/Scripts/MoveUnit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GridSystem))]
+public class MoveUnit : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerUnit;
+    [SerializeField] private List<GameObject> selectUnitList;
+    private GridSystem gridSystem;
+    private Camera cameraMain;
+
+    private void Start()
+    {
+        cameraMain = Camera.main;
+        gridSystem = GetComponent<GridSystem>();
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CommandAddUnit();
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            MoveUnits();
+        }
+    }
+
+    private void CommandAddUnit()
+    {
+        AddUnitToSelectUnitList(GetHitFromLayerUnit().transform.gameObject);
+    }
+    
+    private void AddUnitToSelectUnitList(GameObject other)
+    {
+        if (selectUnitList.Contains(other)) return;
+        selectUnitList.Add(other);
+    }
+    
+    private void MoveUnits()
+    {
+        foreach (var unit in selectUnitList)
+        {
+            gridSystem.MoveUnitToPosition(unit);
+        }
+    }
+    
+    private RaycastHit GetHitFromLayerUnit()
+    {
+        var ray = cameraMain.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out var hit, Mathf.Infinity, layerUnit);
+        return hit;
+    }
+}
